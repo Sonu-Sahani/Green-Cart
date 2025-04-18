61cc3eb8ea9d063139b247324c923a8ffd6b85b2
RishuOg <rishu12shivu@gmail.com>
2025-04-18 15:44:33 +0530

    components and pages were added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Navbar from '../components/Navbar'
+import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
+import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const home = () => {
   return (
     <div>
         <Navbar/>
         <MainBanner/>
+        <Categories/>
+        <BestSeller/>
+        <BottomBanner/>
+        <NewsLetter/>
     </div>
   )
 }
