c26e5749ec9cce9d206424c926d5f6628fb6f110
sonu-sahani <sonusahani.tech@gmail.com>
2026-06-06 12:43:06 +0545

    adding addproduct pages and refining the code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,57 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/home'
-import {Toaster} from "react-hot-toast"
-import { useAppContext } from './context/AppContext'
-import Login from './components/Login'
-import Footer from './components/Footer'
-import AllProducts from './pages/AllProducts'
-import ProductCategory from './pages/ProductCategory'
-import ProductDetails from './pages/ProductDetails'
-import Cart from './pages/Cart'
-import AddAddress from './pages/AddAddress'
-import MyOrders from './pages/MyOrders'
-import SellerLogin from './components/seller/SellerLogin'
-import SellerLayout from './pages/Seller/SellerLayout'
-import AddProduct from './pages/Seller/AddProduct'
-import ProductList from './pages/Seller/ProductList'
-import Orders from './pages/Seller/Orders'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/home";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
+import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from "./pages/Seller/SellerLayout";
+import AddProduct from "./pages/Seller/AddProduct";
+import ProductList from "./pages/Seller/ProductList";
+import Orders from "./pages/Seller/Orders";
 
 const App = () => {
-  const isSellerPath = useLocation().pathname.includes("seller"); 
-  const {showUserLogin, isSeller} = useAppContext()
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin, isSeller } = useAppContext();
   return (
-    <div className='text-default min-h-screen text-grey-700 bg-white'>
-      {isSellerPath ?null: <Navbar />} 
-      {showUserLogin ? <Login/> : null}
-      <Toaster/>
+    <div className="text-default min-h-screen text-grey-700 bg-white">
+      {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
+      <Toaster />
 
-      <div className={`${isSellerPath ? '' : 'px-6 md:px-20 lg:px-24 xl:px-32'}`}>
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-20 lg:px-24 xl:px-32"}`}
+      >
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products' element={<AllProducts />} />
-          <Route path='/products/:category' element={<ProductCategory />} />
-          <Route path='/products/:category/:id' element={<ProductDetails />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/add-address' element={<AddAddress />} />
-          <Route path='/my-orders' element={<MyOrders/>} />
-          <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>} />
-          <Route index element = {isSeller ? <AddProduct/> : null} />
-          <Route path='/product-list' element={<ProductList/>} />
-          <Route path='/orders' element={<Orders/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/products/:category/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/add-address" element={<AddAddress />} />
+          <Route path="/my-orders" element={<MyOrders />} />
+
+          <Route
+            path="/seller"
+            element={isSeller ? <SellerLayout /> : <SellerLogin />}
+          >
+            <Route index element={<AddProduct />} />
+            <Route path="product-list" element={<ProductList />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
         </Routes>
       </div>
-      {!isSellerPath && <Footer/>}
+      {!isSellerPath && <Footer />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
