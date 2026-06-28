313a4e5f5b71edbf7c8a80e3e128a5bfc3d7e35b
sonu-sahani <sonusahani.tech@gmail.com>
2026-06-29 00:45:18 +0545

    updating routes

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB(); // Connect to the database
 await connectCloudinary();//connect to the cloudinary
 
 //allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-azure-gamma.vercel.app'];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
