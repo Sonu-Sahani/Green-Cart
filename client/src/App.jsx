ec06c41d5f476c664eddf0091e543074f5f88d61
sonu-sahani <sonusahani.tech@gmail.com>
2026-06-28 20:07:27 +0545

    UPDATING and integrating online payment service; stripe service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import SellerLayout from "./pages/Seller/SellerLayout";
 import AddProduct from "./pages/Seller/AddProduct";
 import ProductList from "./pages/Seller/ProductList";
 import Orders from "./pages/Seller/Orders";
+import Loading from "./components/loading";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -38,6 +39,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/loader" element={<Loading />} />
 
           <Route
             path="/seller"
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/order.js";
 import Product from "../models/Product.js";
+import stripe from "stripe"
 
 //place order cod: /api/order/cod
 export const placeOrderCOD = async (req, res) => {
@@ -25,6 +26,80 @@ export const placeOrderCOD = async (req, res) => {
             paymentType: 'COD',
         })
         res.json({ success: true, message: "Order placed successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message:error.message})
+    }
+}
+
+//place order Stripe: /api/order/stripe
+export const placeOrderStripe = async (req, res) => {
+    try {
+        const { items,address } = req.body;
+        const userId = req.userId;
+
+        const {origin} = req.headers;
+
+        if(!address || items.length === 0){
+            return res.json({success: false, message:"Please add address and items"})
+        }
+
+        let productData = [];
+
+        //calculate total amount
+        let amount = await items.reduce(async(acc, item)=>{
+            const product = await Product.findById(item.product)
+            productData.push({
+                name: product.name,
+                price: product.offerPrice,
+                quantity: item.quantity,
+            });
+
+            return (await acc) + (product.offerPrice * item.quantity)
+        },0)
+
+        //add tax charge 2%
+        amount += Math.floor(amount * 0.02);
+
+        const order = await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType: 'Online',
+        });
+
+        //stripe gateway initialize
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
+
+        //create line items for stripe
+        const line_items = productData.map((item)=>{
+            return {
+                price_data:{
+                    currency: "usd",
+                    product_data: {
+                        name:item.name,
+                    },
+                    unit_amount: Math.floor(item.price + item.price * 0.02)*100
+                },
+                quantity: item.quantity,
+            }
+        })
+
+        // cretae session
+        const session = await stripeInstance.checkout.sessions.create({
+            line_items,
+            mode:"payment",
+            success_url: `${origin}/loader?next=my-orders`,
+            cancel_url: `${origin}/cart`,
+            metadata:{
+                orderId: order._id.toString(),
+                userId
+            }
+        })
+
+        return res.json({ success: true, url: session.url });
+
     } catch (error) {
         console.log(error.message);
         res.json({success: false, message:error.message})
